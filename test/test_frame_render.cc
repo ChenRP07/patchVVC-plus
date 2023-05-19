4aa7b877ee48daa5eed3d1359cce66e03f72073d
LiXin_ing <1300647670@qq.com>
2023-05-19 10:37:08 +0800

    添加测试文件

--- test/test_frame_render.cc
+++ test/test_frame_render.cc
@@ -1,14 +1,14 @@
 /*
  * @Author: lixin
  * @Date: 2023-05-16 21:12:20
- * @LastEditTime: 2023-05-18 16:09:48
+ * @LastEditTime: 2023-05-19 10:25:48
  * @Description: 
  * Copyright (c) @lixin, All Rights Reserved.
  */
 #include "cuda/manager.h"
 int main()
 {
-   auto &d  = vvc::client::Manager::Init();
+    auto &instance  = vvc::client::Manager::Init();
+    instance.Start(1, "/home/lixin/vvc/test/data/result_1.txt");
     return 0;
-
 }

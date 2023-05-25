3755945d21a4c5b9d6d23a045cdfc9f7664b37da
ChenRP07 <chenrp07@gmail.com>
2023-05-25 11:01:58 +0800

    优化密度分割

--- src/segment/dense_segment.cc
+++ src/segment/dense_segment.cc
@@ -30,7 +30,7 @@ namespace segment {
 
 		// segment old_block_ into two new_block along the max range dimension
 		float max_range;
-		char  div_dim;
+		char div_dim;
 
 		if (max_x - min_x > max_y - min_y) {
 			max_range = max_x - min_x;
@@ -81,78 +81,6 @@ namespace segment {
 		}
 	}
 
-	void DenseSegment::TwoMeans(std::vector<int>& _old_block, std::vector<int>& _new_block_a, std::vector<int>& _new_block_b) {
-		/* Centroid */
-		int   c0{}, c1{};
-		float MaxD{};
-		auto  Dis = [](const pcl::PointXYZRGB& _x, const pcl::PointXYZRGB& _y) -> float { return std::pow(_x.x - _y.x, 2) + std::pow(_x.y - _y.y, 2) + std::pow(_x.z - _y.z, 2); };
-		for (int i = 0; i < _old_block.size(); ++i) {
-			float d = Dis(this->source_cloud_->at(c0), this->source_cloud_->at(_old_block[i]));
-			if (d > MaxD) {
-				c1 = i;
-				MaxD = d;
-			}
-		}
-		float            error{};
-		pcl::PointXYZRGB cp0{this->source_cloud_->at(c0)};
-		pcl::PointXYZRGB cp1{this->source_cloud_->at(c1)};
-
-		for (int iter = 0; iter < this->params_->patch.max_iter; iter++) {
-			pcl::PointXYZRGB cpn0{}, cpn1{};
-			int              cnt0{}, cnt1{};
-
-			for (auto i : _old_block) {
-				float d0 = Dis(cp0, this->source_cloud_->at(i));
-				float d1 = Dis(cp1, this->source_cloud_->at(i));
-				if (d0 > d1) {
-					cpn0.x += this->source_cloud_->at(i).x;
-					cpn0.y += this->source_cloud_->at(i).y;
-					cpn0.z += this->source_cloud_->at(i).z;
-					cnt0++;
-				}
-				else {
-					cpn1.x += this->source_cloud_->at(i).x;
-					cpn1.y += this->source_cloud_->at(i).y;
-					cpn1.z += this->source_cloud_->at(i).z;
-					cnt1++;
-				}
-			}
-
-			cpn0.x /= cnt0;
-			cpn0.y /= cnt0;
-			cpn0.z /= cnt0;
-
-			cpn1.x /= cnt1;
-			cpn1.y /= cnt1;
-			cpn1.z /= cnt1;
-
-			error = (Dis(cpn0, cp0) + Dis(cpn1, cp1)) / 2.0f;
-			cp0 = cpn0, cp1 = cpn1;
-			if (error < this->params_->patch.clustering_err_ths) {
-				break;
-			}
-		}
-
-		for (auto i : _old_block) {
-			float d0 = Dis(cp0, this->source_cloud_->at(i));
-			float d1 = Dis(cp1, this->source_cloud_->at(i));
-			if (d0 < d1) {
-				_new_block_a.emplace_back(i);
-			}
-			else if (d1 < d0) {
-				_new_block_b.emplace_back(i);
-			}
-			else {
-				if (_new_block_a.size() <= _new_block_b.size()) {
-					_new_block_a.emplace_back(i);
-				}
-				else {
-					_new_block_b.emplace_back(i);
-				}
-			}
-		}
-	}
-
 	void DenseSegment::Segment() {
 		try {
 			if (!this->source_cloud_ || this->source_cloud_->empty()) {
@@ -184,7 +112,7 @@ namespace segment {
 			}
 
 			const int kBlockNum = static_cast<int>(this->params_->segment.block_num);
-			float     box_tick = std::max(std::max(max_x - min_x, max_y - min_y), max_z - min_z) / this->params_->segment.block_num;
+			float box_tick = std::max(std::max(max_x - min_x, max_y - min_y), max_z - min_z) / this->params_->segment.block_num;
 
 			/* Cut point cloud into kBlockNum * kBlockNum * kBlockNum blocks */
 			std::vector<VIntPtr> blocks(kBlockNum * kBlockNum * kBlockNum);
@@ -210,7 +138,7 @@ namespace segment {
 #undef Range
 
 			/* Each block has point_num / kPointPerPatch centroids, blocks with larger remainder will gain a extra centroid */
-			std::vector<int>                 centroid_num(blocks.size());
+			std::vector<int> centroid_num(blocks.size());
 			std::vector<std::pair<int, int>> remains(blocks.size());
 			for (int i = 0; i < blocks.size(); ++i) {
 				centroid_num[i] = blocks[i]->size() / kPointPerPatch;
@@ -250,7 +178,7 @@ namespace segment {
 
 				for (int j = 0; j < centroid_num[i]; ++j) {
 					pcl::PointXYZRGB center;
-					auto             temp_block = block_queue.top();
+					auto temp_block = block_queue.top();
 					block_queue.pop();
 					center.x = center.y = center.z = 0.0f;
 					for (auto i : *temp_block) {
@@ -276,41 +204,19 @@ namespace segment {
 			}
 
 			for (int i = 0; i < this->source_cloud_->size(); ++i) {
-				std::vector<int>   index(1);
+				std::vector<int> index(1);
 				std::vector<float> distance(1);
 				kdtree.nearestKSearch(this->source_cloud_->at(i), 1, index, distance);
 				temp_result[index[0]]->emplace_back(i);
 			}
 
-			pcl::PointCloud<pcl::PointXYZRGB>::Ptr final_centroids(new pcl::PointCloud<pcl::PointXYZRGB>());
-
-			std::vector<VIntPtr> final_result;
-
-			VIntPtr mid_result = std::make_shared<std::vector<int>>();
-			for (int i = 0; i < temp_result.size(); ++i) {
-				if (temp_result[i]->size() < kPointPerPatch * (1.0f - NUM_THS)) {
-					mid_result->insert(mid_result->end(), temp_result[i]->begin(), temp_result[i]->end());
-				}
-				else {
-					final_result.emplace_back(temp_result[i]);
-					final_centroids->emplace_back(centroids->at(i));
-				}
-			}
-
-			kdtree.setInputCloud(final_centroids);
-			for (auto i : *mid_result) {
-				std::vector<int>   index(1);
-				std::vector<float> distance(1);
-				kdtree.nearestKSearch(this->source_cloud_->at(i), 1, index, distance);
-				final_result[index[0]]->emplace_back(i);
-			}
 			std::priority_queue<VIntPtr, std::vector<VIntPtr>, decltype(heap_cmp)> segment_queue(heap_cmp);
-			for (auto& i : final_result) {
+			for (auto& i : temp_result) {
 				segment_queue.push(i);
 			}
 
-			/* Split cluster whose size is larger than kPP * 1+1/3 */
-			while (segment_queue.top()->size() > kPointPerPatch * (1.0f + NUM_THS)) {
+			/* Split cluster whose size is larger than kPP */
+			while (segment_queue.top()->size() > kPointPerPatch) {
 				auto temp_seg = segment_queue.top();
 				segment_queue.pop();
 				VIntPtr new_seg_a = std::make_shared<std::vector<int>>();
@@ -321,7 +227,6 @@ namespace segment {
 				segment_queue.push(new_seg_b);
 			}
 
-			/* segmentation */
 			while (!segment_queue.empty()) {
 				auto ptr = segment_queue.top();
 				segment_queue.pop();
@@ -331,11 +236,61 @@ namespace segment {
 				}
 			}
 
-			for (int i = 0; i < this->results_.size(); ++i) {
-				unsigned int color{static_cast<unsigned int>(rand() % 0x00ffffff)};
-				vvc::io::SaveUniqueColorPlyFile("./data/ply/res" + std::to_string(i) + ".ply", this->results_[i], color);
+			bool size_ths = false;
+			int cur_idx = this->results_.size() - 1;
+			double r_ths = std::sqrt(this->params_->icp.radius_search_ths);
+			// double r_ths = std::sqrt(5.0f);
+			int MinPts = 4;
+
+			std::vector<pcl::search::KdTree<pcl::PointXYZRGB>> trees(this->results_.size());
+			for (int i = 0; i < trees.size(); ++i) {
+				trees[i].setInputCloud(this->results_[i]);
 			}
-            /* Try to merge cloud here, min dis < 1 and size + size < 2048 */
+			std::cout << "Total : " << this->results_.size() << std::endl;
+
+			while (true) {
+				int merge_idx = -1;
+				for (int k = cur_idx - 1; k >= 0; --k) {
+					if (this->results_[cur_idx]->size() + this->results_[k]->size() > this->params_->segment.num * 1.2) {
+						continue;
+					}
+					size_ths = true;
+					int p_cnt{};
+					for (auto& p : *this->results_[cur_idx]) {
+						std::vector<int> idxes;
+						std::vector<float> dis;
+						trees[k].radiusSearch(p, r_ths, idxes, dis);
+						if (idxes.size() > MinPts) {
+							p_cnt++;
+						}
+						if (p_cnt > MinPts) {
+							merge_idx = k;
+							break;
+						}
+					}
+				}
+				if (merge_idx != -1) {
+					*this->results_[merge_idx] += *this->results_[cur_idx];
+					this->results_.erase(this->results_.begin() + cur_idx);
+					trees.erase(trees.begin() + cur_idx);
+					trees[merge_idx].setInputCloud(this->results_[merge_idx]);
+				}
+				if (size_ths) {
+					size_ths = false;
+					cur_idx--;
+				}
+				else {
+					break;
+				}
+			}
+
+			// for (int i = 0; i < this->results_.size(); ++i) {
+			// 	unsigned int color{static_cast<unsigned int>(rand() % 0x00ffffff)};
+			// 	vvc::io::SaveUniqueColorPlyFile("./data/ply/res" + std::to_string(i) + ".ply", this->results_[i], color);
+			// 	std::cout << this->results_[i]->size() << std::endl;
+			// }
+			// std::cout << "total : " << this->results_.size() << std::endl;
+			/* Try to merge cloud here, min dis < 1 and size + size < 2048 */
 		}
 		catch (const common::Exception& e) {
 			e.Log();
